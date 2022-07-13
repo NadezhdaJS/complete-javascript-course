7912f718affd78d215700a7a4ff131dae0e251c4
NadezhdaJS <94574119+NadezhdaJS@users.noreply.github.com>
2022-07-13 21:19:19 +0400

    add: 010 task

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,3 +1,13 @@
 let js = 'amazing';
-if (js === 'amazing') alert('JS is FUN!');
-console.log(12 - 2 + 3)
+console.log(12 - 2 + 3);
+
+
+let wizardName = 'Severus';
+console.log(wizardName);
+
+let country = 'Russia';
+console.log(country);
+let continent = 'Eurasia';
+console.log(continent);
+let population = '145 478 097'
+console.log(population);
