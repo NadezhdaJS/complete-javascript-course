4ef0e61471065122cc9b5f01fb6a7303f89fb323
NadezhdaJS <94574119+NadezhdaJS@users.noreply.github.com>
2022-07-14 13:25:19 +0400

    add: 012 task

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,13 +1,23 @@
-let js = 'amazing';
+/* let js = 'amazing';
 console.log(12 - 2 + 3);
 
 
 let wizardName = 'Severus';
 console.log(wizardName);
-
+ */
 let country = 'Russia';
-console.log(country);
+//console.log(country);
 let continent = 'Eurasia';
-console.log(continent);
+//console.log(continent);
 let population = '145 478 097'
-console.log(population);
+//console.log(population);
+
+/* console.log(typeof 'Harry');
+console.log(typeof 12);
+console.log(typeof true); */
+
+let isIsland = true;
+let language;
+console.log(typeof language);
+console.log(typeof isIsland);
+console.log(typeof country);
