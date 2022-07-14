c024d00e746be84bb1f75cc235d34f7113914282
NadezhdaJS <94574119+NadezhdaJS@users.noreply.github.com>
2022-07-14 23:41:54 +0400

    add: Basic Operators task

--- 01-Fundamentals-Part-1/assignments.js
+++ 01-Fundamentals-Part-1/assignments.js
@@ -1,5 +1,14 @@
 const continent = 'Eurasia';
-const country = 'Russia';
+const myCountry = 'Russia';
 let population = 145478097;
 let isIsland = true;
 const language = 'Russian';
+console.log(population / 2);
+population++;
+let finlandPopulation = 6000000;
+console.log(population > finlandPopulation);
+let averagePopulation = 33000000;
+console.log(population < averagePopulation);
+let description = myCountry + ' is in ' + continent + ', and its ' + population + ' people speak ' + language;
+console.log(description);
+
--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -5,19 +5,18 @@ console.log(12 - 2 + 3);
 let wizardName = 'Severus';
 console.log(wizardName);
  */
-let country = 'Russia';
-//console.log(country);
+/* let country = 'Russia';
+
 let continent = 'Eurasia';
-//console.log(continent);
 let population = '145 478 097';
-//console.log(population);
+
 
 /* console.log(typeof 'Harry');
 console.log(typeof 12);
 console.log(typeof true); */
 
-let isIsland = true;
+/* let isIsland = true;
 let language;
 console.log(typeof language);
 console.log(typeof isIsland);
-console.log(typeof country);
+console.log(typeof country);  */
