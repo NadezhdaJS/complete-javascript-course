43fcabda7b9102a56db02a767022383c3fbfadc0
NadezhdaJS <94574119+NadezhdaJS@users.noreply.github.com>
2022-07-15 16:59:49 +0400

    add: template literals task

--- 01-Fundamentals-Part-1/assignments.js
+++ 01-Fundamentals-Part-1/assignments.js
@@ -8,6 +8,7 @@ let finlandPopulation = 6000000;
 console.log(population > finlandPopulation);
 let averagePopulation = 33000000;
 console.log(population < averagePopulation);
-let description = myCountry + ' is in ' + continent + ', and its ' + population + ' people speak ' + language;
+//let description = myCountry + ' is in ' + continent + ', and its ' + population + ' people speak ' + language;
+let description = `${myCountry} is in ${continent}, and its ${population} people speak ${language}`;
 console.log(description);
 
