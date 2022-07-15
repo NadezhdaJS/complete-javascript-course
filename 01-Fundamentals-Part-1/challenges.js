79e56466c152efa3bf9782ecea7df5faacddc9b4
NadezhdaJS <94574119+NadezhdaJS@users.noreply.github.com>
2022-07-15 18:09:37 +0400

    add: Coding Challenge #2

--- 01-Fundamentals-Part-1/challenges.js
+++ 01-Fundamentals-Part-1/challenges.js
@@ -1,14 +1,17 @@
 //1st challenge
-/* let markMass = 78;
-let johnMass = 92;
-let markHeight = 1.69;
-let johnHeight = 1.95; */
-
-/* let markMass = 95;
+let markMass = 95;
 let johnMass = 85;
 let markHeight = 1.88;
 let johnHeight = 1.76;
 const markBMI = markMass / markHeight ** 2;
 const johnBMI = johnMass / johnHeight ** 2;
 const markHigherBMI = markBMI > johnBMI;
-console.log(markBMI, johnBMI, markHigherBMI); */
+console.log(markBMI, johnBMI, markHigherBMI);
+
+//Coding Challenge #2 
+
+if (markBMI > johnBMI) {
+    console.log(`Mark's BMI (${markBMI}) is higher than John's! (${johnBMI})`)
+} else {
+    console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI})!`)
+}
