91ea97e7cd66be4fd1064c979765c3aec5f5665b
NadezhdaJS <94574119+NadezhdaJS@users.noreply.github.com>
2022-07-20 22:28:39 +0400

    Update challenges.js

--- 02-Fundamentals-Part-2/challenges.js
+++ 02-Fundamentals-Part-2/challenges.js
@@ -4,14 +4,14 @@
 
 
 const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
-const avgDolhins = calcAverage(10, 10, 10);
-const avgKoalas = calcAverage(30, 30, 30);
+const avgDolhins = calcAverage(85, 54, 41);
+const avgKoalas = calcAverage(23, 34, 27);
 console.log(avgDolhins, avgKoalas);
 
 function checkWinner(avgDolhins, avgKoalas) {
-    if (avgDolhins > avgKoalas && avgDolhins >= avgKoalas * 2) {
+    if (avgDolhins >= avgKoalas * 2) {
         console.log(`Dolphins win (${avgDolhins} vs. ${avgKoalas})`)
-    } else if (avgKoalas > avgDolhins && avgKoalas >= 2 * avgDolhins) {
+    } else if (avgKoalas >= 2 * avgDolhins) {
         console.log(`Koalas win (${avgKoalas} vs. ${avgDolhins})`)
     }
     else {
