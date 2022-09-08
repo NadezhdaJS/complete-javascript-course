e3c2aa48f453ec3255a7b688591c5fdf4b3f4fff
NadezhdaJS <94574119+NadezhdaJS@users.noreply.github.com>
2022-09-08 17:26:06 +0400

    add: Optional Chaining

--- 05/script2.js
+++ 05/script2.js
@@ -52,3 +52,21 @@ for (const [i, item] of menu.entries()) {
 
 //ES6 Объект 1 можно добавить в объект 2 просто вписав туда переменную: openingHours. Также можно убрать слово function и двоеточие из всех функций внутри объекта и ничего не изменится.
 //Еще можно за пределами объекта создать массив и передать элементы массива во вложенный объект. Или даше посчитать его. Пример: https://i.imgur.com/LYV7YKv.jpeg
+
+////////////////
+//Optional Chaining - Опциональная цепочка
+//console.log(restaurant.openingHours.mon.open); // Выдаст ошибку, так как restaurant.openingHours.mon не существует. Чтобы такой ошибки не было, нужно проверить, существует ли restaurant.openingHours.mon
+if (restaurant.openingHours.mon) console.log(restaurant.openingHours.mon.open); //Здесь ничего не выполнится, и ошибки не будет. Но может быть так, что openingHours тоже не существует и тогда нужна еще проверка:
+if (restaurant.openingHours && restaurant.openingHours.mon)
+  console.log(restaurant.openingHours.mon.open);
+//Но можно сделать следующим способом:
+console.log(restaurant.openingHours?.mon?.open); // (?.) Проверяет, существует ли restaurant.openingHours и .mon и только тогда выполняет open. Если не существует, то выдает undefined (если это не null или undefined)
+let days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun '];
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'another day'; //Если времени открытия не существует, то присвоить значение another day
+  console.log(`On ${day}, we open at ${open}`);
+}
+//Операторы (??) и (.?) появились одновременно в 2020 году и существуют, чтобы работать друг с другом
+console.log(restaurant.order?.(0, 1) ?? `Method does not exist`); //Работает и с методами. Если существует, вызываем его, если нет, то пишем, что он не существует
+let users = [{ name: 'Jonas', email: 'i@mail.ru' }];
+console.log(users[0]?.name ?? `User arr is empty`); //Существует ли name в массиве?
