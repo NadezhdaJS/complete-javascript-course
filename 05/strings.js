cf5ffb74f76aa504af7d2dc74b2fe9dc74e7210f
NadezhdaJS <94574119+NadezhdaJS@users.noreply.github.com>
2022-09-15 22:09:13 +0400

    add: Strings - Part 3

--- 05/strings.js
+++ 05/strings.js
@@ -100,3 +100,53 @@ const checkBaggage = function (items) {
 checkBaggage('I have a laptop, some Food and a pocket Knife');
 checkBaggage('Socks and camera');
 checkBaggage('Got some snacks and a gun for protection');
+// Working With Strings - Part 3
+
+// Split and join
+console.log('a+very+nice+string'.split('+')); //Превращает строку в массив. Разъединяет все между + (обычно используют пробел — " ")
+console.log('Jonas Schmedtmann'.split(' '));
+
+const [firstName, lastName] = 'Jonas Schmedtmann'.split(' ');
+
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' '); //join превращает массив в строку
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(' '); //делаем из строки массив имен
+  const namesUpper = []; //создаем пустой массив, куда будут падать отформатированные конечные имена
+  //создаем цикл каждого имени
+  for (const n of names) {
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase())); //Добавляем в новый массив - меняем первую букву на заглавную
+  }
+  console.log(namesUpper.join(' ')); //возвращаем строку
+};
+
+capitalizeName('jessica ann smith davis');
+capitalizeName('jonas schmedtmann');
+
+// Padding
+const message = 'Go to gate 23!';
+console.log(message.padStart(20, '+').padEnd(30, '+')); //добавляем какое-то количество символов или букв в конец или начало строки пока строка не станет указанного размера-30
+console.log('Jonas'.padStart(20, '+').padEnd(30, '+'));
+
+const maskCreditCard = function (number) {
+  const str = number + ''; // превращаем строку путем конкатенации
+  const last = str.slice(-4); //вырезали последние 4 цифры
+  return last.padStart(str.length, '*'); //остальные заменили звездочками
+};
+
+console.log(maskCreditCard(64637836));
+console.log(maskCreditCard(43378463864647384));
+console.log(maskCreditCard('334859493847755774747'));
+
+// Repeat
+const message2 = 'Bad waether... All Departues Delayed... ';
+console.log(message2.repeat(5)); //повторяет строку 5 раз
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line ${'🛩'.repeat(n)}`);
+};
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
