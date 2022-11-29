43f6acd17c6832e8401a98e688942f632d02155a
NadezhdaJS <94574119+NadezhdaJS@users.noreply.github.com>
2022-11-29 23:00:37 +0400

    add: challenge

--- 07 - Arrays/chall.js
+++ 07 - Arrays/chall.js
@@ -31,5 +31,44 @@ const calcAverageHumanAge = function (ages) {
 
   return Math.round(average / adultDogs.length);
 };
-console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
-console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
+/* console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4])); */
+
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+
+dogs.forEach((element, index) => {
+  dogs[index].recommendedFood = dogs[index].weight ** 0.75 * 28;
+});
+
+let okFood = function (current, recommended) {
+  if (current > recommended * 0.9 && current < recommended * 1.1) {
+    console.log(`Sarah's dog eats good`);
+  } else if (current < recommended) {
+    console.log(`Sarah's dog eats too little`);
+  } else {
+    console.log(`Sarah's dog eats too many`);
+  }
+};
+
+let findSarahDog = function () {};
+
+console.log(dogs);
+let y = dogs[0];
+if (y.owners.find(own => own === 'Bob')) {
+  console.log(`fff`);
+}
+
+dogs.forEach((element, i) => {
+  if (dogs.includes('Sarah')) {
+    console.log(`fff`);
+  }
+});
+
+/* Find Sarah's dog and log to the console whether it's eating too much or too 
+little. Hint: Some dogs have multiple owners, so you first need to find Sarah in 
+the owners array, and so this one is a bit tricky (on purpose) 🤓  */
